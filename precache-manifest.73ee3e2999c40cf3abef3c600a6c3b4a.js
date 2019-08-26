8e322a03867122aa861a03f47273e7fdfa8dd9fe
Gigi1111 <airolg1111@gmail.com>
2019-08-27 05:29:33 +0800

    Updates

---
+++ precache-manifest.73ee3e2999c40cf3abef3c600a6c3b4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d84ce5b2dfbb749981fd0688e2c72f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6a43baaff759713c747f",
+    "url": "/monsters-rolodex/static/css/main.267cc22b.chunk.css"
+  },
+  {
+    "revision": "f478c3e6a5a59dee73f6",
+    "url": "/monsters-rolodex/static/js/2.81f98875.chunk.js"
+  },
+  {
+    "revision": "6a43baaff759713c747f",
+    "url": "/monsters-rolodex/static/js/main.ed33eae0.chunk.js"
+  },
+  {
+    "revision": "9ab012cb21ad84bf1419",
+    "url": "/monsters-rolodex/static/js/runtime~main.d10d2276.js"
+  }
+]);
